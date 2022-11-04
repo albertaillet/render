69d723d13af7a4c1da2104002929b44c261ce254
Albert Sund Aillet <albert@sundaillet.com>
2022-11-04 11:58:00 +0100

    format

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ x0, y0 = 5, 0
 # %%
 range = np.arange(-size, size)
 x, y = np.meshgrid(range, range)
-z0 = (r ** 2 - x0 ** 2 - y0 ** 2)
+z0 = r**2 - x0**2 - y0**2
 z0 = np.sqrt(z0)
 
 # %%
 z = r**2 - x**2 - y**2
 z = np.sqrt(z, where=(z > 0))
-q = (x ** 2 + y ** 2) ** 0.5
-b = (x * x0 + y * y0 + z * z0) / r ** 2
+q = (x**2 + y**2) ** 0.5
+b = (x * x0 + y * y0 + z * z0) / r**2
 fig = px.imshow(b)
 fig.update_layout(
     xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
