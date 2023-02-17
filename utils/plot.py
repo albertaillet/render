9350e0097f43bfc31ddd1ed155666e2248a4ef14
Albert Sund Aillet <albert@sundaillet.com>
2023-02-17 13:36:02 +0100

    added corresponding graph objects code and removed unused arguments

--- utils/plot.py
+++ utils/plot.py
@@ -5,56 +5,49 @@ from PIL import Image
 
 # typing
 from typing import Optional, Tuple
-from jax import Array
+from jaxtyping import Array
 
 
 def imshow(im: Optional[Array] = None, view_size: Tuple[int, int] = (0, 0)) -> dict:
-    h, w = view_size
+    w, h = view_size
+
+    ''' The returned figure is equivalent to the following:
+    return go.Figure(
+        data=go.Image(
+            source=im if im is None else to_base_64(im),
+            hoverinfo='none',
+        ),
+        layout=go.Layout(
+            xaxis=dict(visible=False, range=[0, w]),
+            yaxis=dict(visible=False, range=[0, h], scaleanchor='x'),
+            margin=dict(l=0, r=0, b=0, t=0, pad=0),
+            plot_bgcolor='rgba(0,0,0,0)',
+            paper_bgcolor='rgba(0,0,0,0)',
+            dragmode=False,
+        ),
+    )
+    '''
+
     return {
         'data': [
             {
-                'source': im if im is None else to_base_64(im),
                 'type': 'image',
-                'xref': 'x',
-                'yref': 'y',
-                'x': 0,
-                'y': h,
-                'sizex': w,
-                'sizey': h,
-                'sizing': 'stretch',
-                'layer': 'below',
-                'showscale': False,
+                'source': im if im is None else to_base_64(im),
                 'hoverinfo': 'none',
             },
         ],
         'layout': {
-            'xaxis': {
-                'visible': False,
-                'range': [0, w],
-            },
-            'yaxis': {
-                'visible': False,
-                'range': [0, h],
-                'scaleanchor': 'x',
-            },
-            'margin': {
-                'l': 0,
-                'r': 0,
-                'b': 0,
-                't': 0,
-                'pad': 0,
-            },
+            'xaxis': {'visible': False, 'range': [0, w]},
+            'yaxis': {'visible': False, 'range': [0, h]},
+            'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0, 'pad': 0},
             'plot_bgcolor': 'rgba(0,0,0,0)',
             'paper_bgcolor': 'rgba(0,0,0,0)',
             'dragmode': False,
-            'height': '100vh',
-            'width': '100vw',
         },
     }
 
 
 def to_base_64(im: Array) -> str:
-    im = Image.fromarray(np.asarray(im))
     with io.BytesIO() as buffer:
-        im.save(buffer, format='png')
+        Image.fromarray(np.asarray(im)).save(buffer, format='png')
         return "data:image/png;base64," + base64.b64encode(buffer.getvalue()).decode()
