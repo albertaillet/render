8ba0f6b1af475b5e85879921ba95515c8ebfa8dc
Albert Sund Aillet <albert@sundaillet.com>
2023-02-17 13:36:35 +0100

    added imprort and changed to tuple

--- utils/plot.py
+++ utils/plot.py
@@ -12,14 +12,15 @@ def imshow(im: Optional[Array] = None, view_size: Tuple[int, int] = (0, 0)) -> d
     w, h = view_size
 
     ''' The returned figure is equivalent to the following:
+    from plotly import graph_objects as go
     return go.Figure(
         data=go.Image(
             source=im if im is None else to_base_64(im),
             hoverinfo='none',
         ),
         layout=go.Layout(
-            xaxis=dict(visible=False, range=[0, w]),
-            yaxis=dict(visible=False, range=[0, h], scaleanchor='x'),
+            xaxis=dict(visible=False, range=(0, w)),
+            yaxis=dict(visible=False, range=(0, h)),
             margin=dict(l=0, r=0, b=0, t=0, pad=0),
             plot_bgcolor='rgba(0,0,0,0)',
             paper_bgcolor='rgba(0,0,0,0)',
@@ -37,8 +38,8 @@ def imshow(im: Optional[Array] = None, view_size: Tuple[int, int] = (0, 0)) -> d
             },
         ],
         'layout': {
-            'xaxis': {'visible': False, 'range': [0, w]},
-            'yaxis': {'visible': False, 'range': [0, h]},
+            'xaxis': {'visible': False, 'range': (0, w)},
+            'yaxis': {'visible': False, 'range': (0, h)},
             'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0, 'pad': 0},
             'plot_bgcolor': 'rgba(0,0,0,0)',
             'paper_bgcolor': 'rgba(0,0,0,0)',
