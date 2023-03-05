7a39346d9fa972cd8483d886e4653efee6b265ed
Albert Sund Aillet <albert@sundaillet.com>
2023-03-05 22:17:41 +0100

    added code for optimizing on one image

--- test.py
+++ test.py
@@ -15,13 +15,25 @@ from raymarch import (
 from functools import partial
 import optax
 from tqdm import tqdm
+import numpy as onp
+from PIL import Image
 
 # typing
 from typing import Tuple, Sequence, NamedTuple, Callable
 from jax import Array
 
 VIEW_SIZE = (100, 100)
-render_scene = partial(render_scene, view_size=VIEW_SIZE, click=(-1, -1), light_dir=LIGHT_DIR)
+render_scene = partial(
+    render_scene, view_size=VIEW_SIZE, click=(-1, -1), light_dir=LIGHT_DIR
+)
+
+# %%
+gt_pil_img = Image.open("snowman.png").resize(VIEW_SIZE).transpose(Image.FLIP_LEFT_RIGHT)
+transparency = np.array(gt_pil_img)[:, :, 4] == 0
+
+gt_img = np.float32(gt_pil_img)[:, :, :3] / 255.0
+gt_img = np.where(transparency[:, :, None], 1.0, gt_img)
+gt_pil_img
 
 # %%
 camera = Camera(
@@ -30,21 +42,13 @@ camera = Camera(
     target=np.array([0.0, 0.0, 0.0]),
 )
 
-spheres = Spheres(
-    position=np.array([[0.0, 0.6, 0.0], [0.0, 2.0, 0.0]]),
-    radius=np.array([1.0, 0.5]),
-    color=np.array([[1.0, 0.0, 0.0], [0.0, 0.0, 1.0]]),
-)
-
 planes = Planes(
     position=np.zeros((1, 3)),
     normal=np.array([[0.0, 1.0, 0.0]]),
     color=np.ones((1, 3)),
 )
 
-gt_scene = Scene((spheres, planes))
 
-# %%
 class LearntObjects(NamedTuple):
     position: Array
     radius: Array
@@ -52,13 +56,13 @@ class LearntObjects(NamedTuple):
 
     def sdf(self, p: Array) -> Array:
         return norm(p - self.position) - self.radius
-    
+
     def colorf(self, p: Array) -> Array:
         n = self.radius.shape[0]
         c = sigmoid(self.color(p))
         c = np.tile(c, (n, 1))
         return c
-    
+
 
 p_key, r_key, c_key = random.split(random.PRNGKey(0), 3)
 
@@ -66,14 +70,13 @@ n = 10
 p_scale = 2.0
 r_scale = 0.1
 learnt_objects = LearntObjects(
-    position=p_scale*random.normal(p_key, (n, 3)),
+    position=p_scale * random.normal(p_key, (n, 3)),
     radius=r_scale * random.uniform(r_key, (n,)),
     color=MLP(init_mlp_params([3, 32, 32, 3], key=c_key)),
 )
 learnt_scene = Scene((learnt_objects, planes))
 
 # %%
-gt_img = render_scene(gt_scene, camera)
 plt.imshow(gt_img)
 plt.show()
 learnt_img = render_scene(learnt_scene, camera)
@@ -93,36 +96,39 @@ def loss_fn(
     loss = np.mean(np.abs(image - gt_image))
     return loss, image
 
+
 @jit
 def update_fn(
     scene: Scene,
     camera: Camera,
     gt_image: Array,
     opt_state: optax.OptState,
-) -> Tuple[Scene, optax.OptState]:
-    (loss, learnt_img), grads = value_and_grad(loss_fn, has_aux=True)(scene, camera, gt_image)
+) -> Tuple[Scene, Array, Array, optax.OptState]:
+    (loss, learnt_img), grads = value_and_grad(loss_fn, has_aux=True)(
+        scene, camera, gt_image
+    )
     updates, opt_state = opt_update(grads, opt_state)
     scene = optax.apply_updates(scene, updates)
     return scene, loss, learnt_img, opt_state
 
+
 # %%
-lr = 0.01
+lr = 0.001
 opt_init, opt_update = optax.adam(lr)
 opt_state = opt_init(learnt_scene)
-gt_img = render_scene(gt_scene, camera)
 frames = []
 
 # %%
-pbar = tqdm(range(100))
+pbar = tqdm(range(1_000))
 for i in pbar:
-    learnt_scene, loss, learnt_img, opt_state = update_fn(learnt_scene, camera, gt_img, opt_state)
+    learnt_scene, loss, learnt_img, opt_state = update_fn(
+        learnt_scene, camera, gt_img, opt_state
+    )
     pbar.set_description(f"loss: {loss:.4f}")
     frames.append(learnt_img)
 
 # %%
-import numpy as onp
-from PIL import Image
 imgs = [Image.fromarray((255 * onp.asarray(img)).astype(np.uint8)) for img in frames]
-imgs[0].save("out3.gif", save_all=True, append_images=imgs[1:], duration=100, loop=0)
+imgs[0].save("snowman.gif", save_all=True, append_images=imgs[1:], duration=100, loop=0)
 
 # %%
