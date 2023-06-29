added view choice, rotation, smoothing, corrected box sdf

builder.py
@@ -3,14 +3,32 @@ Expected scene dict format:
 {
     'width': 800
     'height': 600
+    'smoothing': 0.125
     'Camera': {
         'position': [0, 0, 5]
         'target': [0, 0, 0]
         'up': [0, 1, 0]
+        'f': 0.6
     }
     'Objects': [
-        {'Sphere': {'position': [0, 0, 0], 'objattr': [0.5, 0, 0], 'color': [0, 0, 1]}},
-        {'Plane': {'position': [0, 0, 0], 'objattr': [0, 1, 0], 'color': [1, 1, 1]}},
+        {
+            'Sphere': {
+                'position': [0, 0, 0],
+                'attribute': [0.5, 0, 0],
+                'color': [0, 0, 1],
+                'rotation': [0, 0, 0],
+                'rounding': 0.0
+            }
+        },
+        {
+            'Plane': {
+                'position': [0, 0, 0],
+                'attribute': [0, 1, 0],
+                'color': [1, 1, 1]
+                'rotation': [0, 0, 0],
+                'rounding': 0.0
+            }
+        },
     ]
 }
 '''
@@ -19,7 +37,21 @@ from jax import tree_map, numpy as np
 
 # typing
 from typeguard import check_type
-from typing import Tuple, Any, Set
+from typing import Dict, Tuple, Any
+
+CAMERA_FIELDS = {
+    'position': Tuple[float, float, float],
+    'target': Tuple[float, float, float],
+    'up': Tuple[float, float, float],
+    'f': float,
+}
+OBJECT_FIELDS = {
+    'position': Tuple[float, float, float],
+    'attribute': Tuple[float, float, float],
+    'color': Tuple[float, float, float],
+    'rotation': Tuple[float, float, float],
+    'rounding': float,
+}
 
 
 def is_leaf(node: Any) -> bool:
@@ -30,30 +62,26 @@ def build_scene(scene_dict: dict) -> Tuple[rm.Scene, Tuple[int, int]]:
     '''Create a scene from a dict of expected format (see top of file)'''
 
     view_size = scene_dict['height'], scene_dict['width']
+    smoothing = scene_dict['smoothing']
     camera_dict = scene_dict['Camera']
     object_dict_list = scene_dict['Objects']
 
     camera = rm.Camera(**tree_map(np.float32, camera_dict, is_leaf=is_leaf))
 
-    objects = []
-    positions = []
-    objattr = []
-    colors = []
+    object_args = {arg + 's': [] for arg in OBJECT_FIELDS}
+    object_args['objects'] = []
     for obj_dict in object_dict_list:
         obj_type, obj = next(iter(obj_dict.items()))
         idx = rm.OBJECT_IDX[obj_type]
-        objects.append(idx)
-        positions.append(obj['position'])
-        objattr.append(obj['objattr'])
-        colors.append(obj['color'])
+        object_args['objects'].append(idx)
+        for arg in OBJECT_FIELDS:
+            object_args[arg + 's'].append(obj[arg])
 
     return (
         rm.Scene(
-            objects=np.array(objects),
-            positions=np.array(positions),
-            objattr=np.array(objattr),
-            colors=np.array(colors),
+            **tree_map(np.array, object_args, is_leaf=is_leaf),
             camera=camera,
+            smoothing=smoothing,
         ),
         view_size,
     )
@@ -65,24 +93,29 @@ def check_scene_dict(scene_dict: dict) -> None:
         check_type(argname, scene_dict.get(argname), int)
         assert scene_dict[argname] > 0, f'{argname} must be positive'
 
-    check_fields(scene_dict.get('Camera'), {'position', 'target', 'up'})
+    check_type('smoothing', scene_dict.get('smoothing'), float)
+
+    check_fields(scene_dict.get('Camera'), CAMERA_FIELDS)
 
     for outer_obj_dict in scene_dict.get('Objects'):
         for obj_type, obj in outer_obj_dict.items():
             assert obj_type in rm.OBJECT_IDX, f'Unknown object type {obj_type}'
-            check_fields(obj, {'position', 'objattr', 'color'})
+            check_fields(obj, OBJECT_FIELDS)
 
 
-def check_fields(obj: dict, required: Set[str]) -> None:
+def check_fields(obj: dict, fields: Dict[str, type]) -> None:
     check_type('obj', obj, dict)
 
     provided = set(obj.keys())
+    required = set(fields.keys())
     if provided != required:
         raise ValueError(f'{obj} has {provided} fields and should have {required}')
 
-    for field in obj:
+    for argname, argtype in fields.items():
         # cast lists to tuples to be able to check length
-        check_type(field, tuple(obj[field]), Tuple[float, float, float])
+        arg = obj[argname]
+        arg = tuple(arg) if isinstance(arg, list) else arg
+        check_type(argname, arg, argtype)
 
 
 if __name__ == '__main__':

raymarch.py
@@ -1,15 +1,16 @@
 from jax import numpy as np
 from jax import lax, vmap, grad, jit
 from functools import partial
-from utils.linalg import norm, normalize, softmax, relu, min, smoothmin
+from utils.linalg import norm, normalize, softmax, relu, smoothmin, Rxyz
 
 # typing
 from typing import Callable, Tuple, Dict, NamedTuple
-from jaxtyping import Array, Float, Int8
+from jaxtyping import Array, Float, Int
 
 Vec3 = Float[Array, '3']
 Vec3s = Float[Array, 'n 3']
 Scalar = Float[Array, '']
+Scalars = Float[Array, 'n']
 
 
 def sdf_sphere(position: Vec3, radius: Vec3, p: Vec3) -> Scalar:
@@ -24,7 +25,7 @@ def sdf_plane(position: Vec3, normal: Vec3, p: Vec3) -> Scalar:
 
 def sdf_box(position: Vec3, size: Vec3, p: Vec3) -> Scalar:
     q = np.abs(p - position) - size
-    return norm(relu(q)) + min(relu(q))
+    return norm(relu(q)) + np.minimum(np.max(q), 0)
 
 
 OBJECT_IDX = {
@@ -39,14 +40,15 @@ class Camera(NamedTuple):
     up: Vec3
     position: Vec3
     target: Vec3
+    f: float = 0.6
 
-    def rays(self, view_size: Tuple[int, int], fx: float = 0.6) -> Vec3s:
+    def __call__(self, view_size: Tuple[int, int]) -> Vec3s:
         forward = self.target - self.position
         right = np.cross(forward, self.up)
         down = np.cross(right, forward)
         R = normalize(np.vstack([right, down, forward]))
         h, w = view_size
-        fy = fx / w * h
+        fx, fy = self.f, self.f * h / w
         x = np.linspace(-fx, fx, w)
         y = np.linspace(fy, -fy, h)
         x, y = np.meshgrid(x, y)
@@ -56,25 +58,36 @@ class Camera(NamedTuple):
 
 
 class Scene(NamedTuple):
-    objects: Int8
+    objects: Scalars
     positions: Vec3s
-    objattr: Vec3
+    attributes: Vec3s
+    rotations: Vec3s
     colors: Vec3s
+    roundings: Scalars
+    smoothing: float
     camera: Camera
 
-    def sdf(self, p: Array) -> Array:
-        def switch(obj_idx, pos, attr):
-            return lax.switch(obj_idx, BRANCHES, pos, attr, p)
+    def sdfs(self, p: Vec3) -> Scalars:
+        def switch(p: Vec3, obj_idx: Int, pos: Vec3, attr: Vec3, rot: Vec3):
+            p_rot = (p - pos) @ Rxyz(rot) + pos
+            return lax.switch(obj_idx, BRANCHES, pos, attr, p_rot)
 
-        return vmap(switch)(self.objects, self.positions, self.objattr)
+        dists = vmap(partial(switch, p))(
+            self.objects,
+            self.positions,
+            self.attributes,
+            self.rotations,
+        )
+        return dists - self.roundings
 
-    def color(self, p: Array) -> Array:
-        dists = self.sdf(p)
-        color = softmax(-8.0 * dists) @ self.colors
-        return color
+    def sdf(self, p: Vec3) -> Scalar:
+        return smoothmin(self.sdfs(p), self.smoothing)
+
+    def color(self, p: Vec3) -> Scalar:
+        return softmax(-self.sdfs(p) / self.smoothing) @ self.colors
 
 
-def raymarch(sdf: Callable, p0: Array, dir: Array, n_steps: int = 50) -> Array:
+def raymarch(sdf: Callable, p0: Vec3, dir: Vec3, n_steps: int = 50) -> Vec3:
     def march_step(_, p):
         return p + sdf(p) * dir
 
@@ -82,8 +95,8 @@ def raymarch(sdf: Callable, p0: Array, dir: Array, n_steps: int = 50) -> Array:
 
 
 def cast_shadow(
-    sdf: Callable, light_dir: Array, p0: Array, n_steps: int = 50, hardness: float = 4.0
-) -> Array:
+    sdf: Callable, light_dir: Vec3, p0: Vec3, n_steps: int = 50, hardness: float = 4.0
+) -> Scalar:
     def shade_step(_, carry):
         t, shadow = carry
         h = sdf(p0 + light_dir * t)
@@ -94,15 +107,15 @@ def cast_shadow(
 
 
 def shade_f(
-    surface_color: Array,
-    raw_normal: Array,
-    ray_dir: Array,
-    shadow: Array,
-    light_dir: Array,
-) -> Array:
+    surface_color: Vec3,
+    raw_normal: Vec3,
+    ray_dir: Vec3,
+    shadow: Vec3,
+    light_dir: Vec3,
+) -> Scalar:
     ambient = norm(raw_normal)
     normal = raw_normal / ambient
-    diffuse = normal.dot(light_dir).clip(0.0) * shadow
+    diffuse = light_dir.dot(normal).clip(0.0) * shadow
     half = normalize(light_dir - ray_dir)
     spec = 0.3 * shadow * half.dot(normal).clip(0.0) ** 200.0
     light = 0.8 * diffuse + 0.2 * ambient
@@ -117,22 +130,19 @@ def render_scene(
     scene: Scene,
     view_size: Tuple[int, int],
     click: Tuple[int, int],
-    light_dir: Array = LIGHT_DIR,
+    light_dir: Vec3 = LIGHT_DIR,
 ) -> Dict[str, Array]:
     h, w = view_size
     i, j = click
-    ray_dir = scene.camera.rays(view_size)
+    ray_dir = scene.camera(view_size)
 
-    def sdf(p: Array) -> Array:
-        return smoothmin(scene.sdf(p), axis=0)
-
-    hit_pos = vmap(partial(raymarch, sdf, scene.camera.position))(ray_dir)
+    hit_pos = vmap(partial(raymarch, scene.sdf, scene.camera.position))(ray_dir)
     surface_color = vmap(scene.color)(hit_pos)
-    raw_normal = vmap(grad(sdf))(hit_pos)
+    raw_normal = vmap(grad(scene.sdf))(hit_pos)
 
     light_dir = np.where(i == -1, light_dir, raw_normal[i * w + j])
     light_dir = normalize(light_dir)
-    shadow = vmap(partial(cast_shadow, sdf, light_dir))(hit_pos)
+    shadow = vmap(partial(cast_shadow, scene.sdf, light_dir))(hit_pos)
     image = vmap(partial(shade_f, light_dir=light_dir))(surface_color, raw_normal, ray_dir, shadow)
 
     image = image ** (1.0 / 2.2)  # gamma correction
@@ -141,8 +151,9 @@ def render_scene(
 
     return {
         'image': image.reshape((h, w, 3)),
-        'raw_normal': raw_normal.reshape((h, w, 3)),
-        'distance': distances.reshape((h, w)),
+        'normal': normalize(raw_normal).reshape((h, w, 3)),
+        'coordinate': (hit_pos % 1.0).reshape((h, w, 3)),
+        'distance': (distances / distances.max()).reshape((h, w)),
     }
 
 

utils/linalg.py
@@ -12,12 +12,8 @@ def normalize(x: Array, axis: int = -1, eps: float = 1e-20) -> Array:
     return x / norm(x, axis=axis, keepdims=True, eps=eps)
 
 
-def min(x: Array, axis: int = -1, keepdims: bool = False) -> Array:
-    return np.min(x, axis=axis, keepdims=keepdims)
-
-
-def smoothmin(x: Array, c: float = 8.0, **kwargs) -> Array:
-    return -nn.logsumexp(-c * x, **kwargs) / c
+def smoothmin(x: Array, c: float = 0.125, **kwargs) -> Array:
+    return -c * nn.logsumexp(-x / c, **kwargs)
 
 
 def softmax(x: Array, *args, **kwargs) -> Array:
@@ -26,3 +22,23 @@ def softmax(x: Array, *args, **kwargs) -> Array:
 
 def relu(x: Array, *args, **kwargs) -> Array:
     return nn.relu(x, *args, **kwargs)
+
+
+def Rx(theta: float) -> Array:
+    c, s = np.cos(theta), np.sin(theta)
+    return np.array([[1, 0, 0], [0, c, -s], [0, s, c]])
+
+
+def Ry(theta: float) -> Array:
+    c, s = np.cos(theta), np.sin(theta)
+    return np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+
+
+def Rz(theta: float) -> Array:
+    c, s = np.cos(theta), np.sin(theta)
+    return np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
+
+
+def Rxyz(rotation_angles: Array) -> Array:
+    theta, phi, psi = rotation_angles
+    return Rz(psi) @ Ry(phi) @ Rx(theta)