830a238ab0dd443ee7b63ee12dd5e36e38f99b10
Albert Sund Aillet <albert@sundaillet.com>
2023-12-26 22:21:48 +0200

    fixed rays seem ok

--- sandbox_tiny_nerf.py
+++ sandbox_tiny_nerf.py
@@ -3,42 +3,65 @@ import numpy as np
 from numpy import load
 
 # from jax import numpy as np
-# from plotly import graph_objects as go
-from matplotlib import pyplot as plt
+from plotly import graph_objects as go
+
+# from matplotlib import pyplot as plt
 
 # %% tiny nerf: https://cseweb.ucsd.edu//~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
 data = load('assets/other/tiny_nerf_data.npz')
-coord_names = 'xyz'
-
-
 print(data['images'].shape)
 print(data['poses'].shape)
-# print(focal)
 
-focal = 100 / data['focal']
-h, w = 2, 2
 
-x = np.linspace(-focal, focal, w)
-y = np.linspace(focal, -focal, h)
-x, y = np.meshgrid(x, y)
-x, y = x.flatten(), y.flatten()
-unit_rays = np.stack((x, y, np.ones(w * h)), axis=-1)
-unit_rays = unit_rays / np.sqrt(np.square(unit_rays).sum(axis=-1, keepdims=True))
+# %%
+def get_unit_rays_linspace(h, w, focal):
+    f = 100 / focal
+    x = np.linspace(-f, f, w)
+    y = np.linspace(f, -f, h)
+    x, y = np.meshgrid(x, y)
+    x, y = x.flatten(), y.flatten()
+    unit_rays = np.stack((x, y, np.ones(w * h)), axis=-1)
+    unit_rays = unit_rays / np.linalg.norm(unit_rays, axis=-1, keepdims=True)
+    return unit_rays
 
-fig, ax = plt.subplots(1, 3, figsize=(9, 3))
-for m in data['poses']:
-    rot = m[:3, :3]
-    pos = m[:3, -1]
-    v = (unit_rays[:, None, :] * rot[None, :, :]).sum(axis=-1)
-    dir = pos[None, 1] + v
-    coord = lambda i, j: [pos[i % 3], dir[j, i % 3]]  # noqa: E731
 
+def get_unit_rays_arange(h, w, focal):
+    # Create a 2D rectangular grid for the rays corresponding to image dimensions
+    i, j = np.meshgrid(np.arange(w), np.arange(h), indexing="xy")
+    i, j = i.flatten(), j.flatten()
+    transformed_i = (i - w * 0.5) / focal  # Normalize the x-axis coordinates
+    transformed_j = -(j - h * 0.5) / focal  # Normalize the y-axis coordinates
+    k = -np.ones_like(i)  # z-axis coordinates
+    # Create the unit vectors corresponding to ray directions
+    unit_rays = np.stack([transformed_i, transformed_j, k], axis=-1)
+    unit_rays = unit_rays / np.linalg.norm(unit_rays, axis=-1, keepdims=True)
+    return unit_rays
+
+
+def get_rays(unit_rays, pose):
+    # unit_rays is assumed to be normalized already
+    rays = np.einsum("il,kl", unit_rays, pose[:3, :3])
+    return rays
+
+
+h, w = 30, 30
+unit_rays = get_unit_rays_linspace(h, w, data["focal"])
+empty = [None] * 3
+traces = []
+traces.append(go.Scatter3d(x=[0], y=[0], z=[0], mode="markers"))
+poses = data["poses"][:20]
+
+for pose in poses:
+    coords = []
+    rays = get_rays(unit_rays, pose)
+    ray_origin = pose[:3, -1]
     for j in range(h * w):
-        for i in range(3):
-            ax[i].plot(coord(j, i), coord(j, i + 1), label=f'{j}')
-for i in range(3):
-    title = coord_names[i] + coord_names[(i + 1) % 3]
-    ax[i].set_title(title)
-plt.show()
+        coords.extend([ray_origin, rays[j], empty])
 
+    x_coords = [x for x, _, _ in coords]
+    y_coords = [y for _, y, _ in coords]
+    z_coords = [z for _, _, z in coords]
+    traces.append(go.Scatter3d(x=x_coords, y=y_coords, z=z_coords, mode="lines"))
+
+go.Figure(traces)
 # %%
