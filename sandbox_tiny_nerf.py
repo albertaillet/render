3812f3b2eb1cc963e0260a95de05636841b6d17a
Albert Sund Aillet <albert@sundaillet.com>
2023-12-26 22:47:27 +0200

    fixes to check that the rays are correct

--- sandbox_tiny_nerf.py
+++ sandbox_tiny_nerf.py
@@ -1,26 +1,22 @@
 # %%
 import numpy as np
-from numpy import load
-
-# from jax import numpy as np
 from plotly import graph_objects as go
 
-# from matplotlib import pyplot as plt
 
 # %% tiny nerf: https://cseweb.ucsd.edu//~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
-data = load('assets/other/tiny_nerf_data.npz')
-print(data['images'].shape)
-print(data['poses'].shape)
+data = np.load('assets/other/tiny_nerf_data.npz')
+print(*[f'{k}: {data[k].shape},' for k in data.keys()])
 
 
 # %%
 def get_unit_rays_linspace(h, w, focal):
-    f = 100 / focal
-    x = np.linspace(-f, f, w)
-    y = np.linspace(f, -f, h)
-    x, y = np.meshgrid(x, y)
+    bx = 0.5 * w / focal
+    by = 0.5 * h / focal
+    x = np.linspace(bx, -bx, w)
+    y = np.linspace(-by, by, h)
+    x, y = np.meshgrid(x, y, indexing="xy")
     x, y = x.flatten(), y.flatten()
-    unit_rays = np.stack((x, y, np.ones(w * h)), axis=-1)
+    unit_rays = np.stack((x, y, -np.ones(w * h)), axis=-1)
     unit_rays = unit_rays / np.linalg.norm(unit_rays, axis=-1, keepdims=True)
     return unit_rays
 
@@ -44,24 +40,31 @@ def get_rays(unit_rays, pose):
     return rays
 
 
-h, w = 30, 30
-unit_rays = get_unit_rays_linspace(h, w, data["focal"])
-empty = [None] * 3
-traces = []
-traces.append(go.Scatter3d(x=[0], y=[0], z=[0], mode="markers"))
+def get_pose_fig(unit_rays, poses):
+    empty = [None] * 3
+    traces = []
+    traces.append(go.Scatter3d(x=[0], y=[0], z=[0], mode="markers"))
+
+    for pose in poses:
+        coords = []
+        rays = get_rays(unit_rays, pose)
+        ray_origin = pose[:3, -1]
+        for j in range(len(rays)):
+            coords.extend([ray_origin, rays[j], empty])
+
+        x_coords = [x for x, _, _ in coords]
+        y_coords = [y for _, y, _ in coords]
+        z_coords = [z for _, _, z in coords]
+        traces.append(go.Scatter3d(x=x_coords, y=y_coords, z=z_coords, mode="lines"))
+
+    return go.Figure(traces)
+
+
+h, w = 100, 100
 poses = data["poses"][:20]
+unit_rays = get_unit_rays_arange(h, w, data["focal"])
+get_pose_fig(unit_rays, poses).show()
+unit_rays = get_unit_rays_linspace(h, w, data["focal"])
+get_pose_fig(unit_rays, poses).show()
 
-for pose in poses:
-    coords = []
-    rays = get_rays(unit_rays, pose)
-    ray_origin = pose[:3, -1]
-    for j in range(h * w):
-        coords.extend([ray_origin, rays[j], empty])
-
-    x_coords = [x for x, _, _ in coords]
-    y_coords = [y for _, y, _ in coords]
-    z_coords = [z for _, _, z in coords]
-    traces.append(go.Scatter3d(x=x_coords, y=y_coords, z=z_coords, mode="lines"))
-
-go.Figure(traces)
 # %%
